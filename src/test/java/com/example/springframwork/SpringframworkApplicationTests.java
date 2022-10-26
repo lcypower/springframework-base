74967f9e1d2804b5430fc5628cc9cd9959d61597
lcypower <lcypower0@gmail.com>
2022-10-26 15:36:41 +0900

    project init

---
+++ src/test/java/com/example/springframwork/SpringframworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springframwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
